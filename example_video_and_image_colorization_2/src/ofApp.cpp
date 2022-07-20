94a47e8a7b91c6a309bf93c583cca492560993ad
Jonathan <41275844+Jonathhhan@users.noreply.github.com>
2022-07-21 00:47:30 +0200

    Add files via upload

---
+++ example_video_and_image_colorization_2/src/ofApp.cpp
@@ -0,0 +1,141 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	ofSetFrameRate(60);
+	ofSetVerticalSync(true);
+	ofSetWindowTitle("example_video_and_image_colorization");
+
+	if (!ofxTF2::setGPUMaxMemory(ofxTF2::GPU_PERCENT_70, true)) {
+		ofLogError() << "failed to set GPU Memory options!";
+	}
+
+	if (!model.load("model")) {
+		std::exit(EXIT_FAILURE);
+	}
+	model.setup({ "serving_default_input_1" }, { "StatefulPartitionedCall" });
+
+#ifdef USE_VIDEO
+	videoPlayer.load("Godard.mp4");
+	imgOut.allocate(videoPlayer.getWidth(), videoPlayer.getHeight(), OF_IMAGE_COLOR);
+	width = videoPlayer.getWidth();
+	height = videoPlayer.getHeight();
+	videoPlayer.play();
+#else
+	imgIn.load("wald.jpg");
+	imgOut.allocate(imgIn.getWidth(), imgIn.getHeight(), OF_IMAGE_COLOR);
+	width = imgIn.getWidth();
+	height = imgIn.getHeight();
+	imgMat = ofxCv::toCv(imgIn);
+	if (imgIn.getImageType() == OF_IMAGE_GRAYSCALE) {
+		cv::cvtColor(imgMat, imgMat, CV_GRAY2RGB);
+	}
+	cv::cvtColor(imgMat, imgMat, CV_RGB2Lab);
+	imgIn.update();
+	input = ofxTF2::pixelsToTensor(imgIn.getPixels().getChannel(0));
+	input = cppflow::cast(input, TF_UINT8, TF_FLOAT);
+	input = cppflow::expand_dims(input, 0);
+	input_resized = cppflow::resize_bicubic(input, cppflow::tensor({ 256, 256 }), true);
+
+	output = model.runModel(input_resized);
+	output = cppflow::div(output, cppflow::tensor({ 1.f / 255.f }));
+	output = cppflow::resize_bicubic(output, cppflow::tensor({ height, width }), true);
+	vectorOfInputTensors = { input, output };
+	output = cppflow::concat(cppflow::tensor({ 3 }), vectorOfInputTensors);
+	ofxTF2::tensorToImage(output, imgOut);
+	imgMat = ofxCv::toCv(imgOut);
+	cv::cvtColor(imgMat, imgMat, CV_Lab2RGB);
+	imgOut.update();
+	imgOut.save("wald_colorized.jpg");
+#endif
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+#ifdef USE_VIDEO
+	videoPlayer.update();
+	if (videoPlayer.isFrameNew()) {
+		input = ofxTF2::pixelsToTensor(videoPlayer.getPixels());
+		input = cppflow::cast(input, TF_UINT8, TF_FLOAT);
+		input = cppflow::div(input, cppflow::tensor({ 255.f }));
+		ofxTF2::tensorToImage(input, imgOut);
+		imgMat = ofxCv::toCv(imgOut);
+		cv::cvtColor(imgMat, imgMat, CV_RGB2Lab);
+		imgOut.update();
+
+		input = ofxTF2::pixelsToTensor(imgOut.getPixels().getChannel(0));
+		input = cppflow::expand_dims(input, 0);
+		input_resized = cppflow::resize_bicubic(input, cppflow::tensor({ 256, 256 }), true);
+
+		output = model.runModel(input_resized);
+		output = cppflow::div(output, cppflow::tensor({ 1.f / 255.f }));
+		output = cppflow::resize_bicubic(output, cppflow::tensor({ height, width }), true);
+		vectorOfInputTensors = { input, output };
+		output = cppflow::concat(cppflow::tensor({ 3 }), vectorOfInputTensors);	
+		ofxTF2::tensorToImage(output, imgOut);
+		imgMat = ofxCv::toCv(imgOut);
+		cv::cvtColor(imgMat, imgMat, CV_Lab2RGB);
+		imgOut.update();
+	}
+#endif
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	imgOut.draw(20, 20, 480, 360);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
---
+++ example_video_and_image_colorization_2/src/ofApp.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxTensorFlow2.h"
+#include "ofxCv.h"
+
+#define USE_VIDEO
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	ofxTF2::Model model;
+	cppflow::tensor input;
+	cppflow::tensor input_resized;
+	cppflow::tensor output;
+	std::vector<cppflow::tensor> vectorOfInputTensors;
+
+#ifdef USE_VIDEO
+	ofVideoPlayer videoPlayer;
+#else
+	ofFloatImage imgIn;
+#endif
+
+	cv::Mat imgMat;
+	ofFloatImage imgOut;
+	int width;
+	int height;
+};
